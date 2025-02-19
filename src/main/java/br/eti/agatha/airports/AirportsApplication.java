401725400969abf6b440314938699fd9140eacd7
agatha <agatha.napoleao14@gmail.com>
2025-02-19 11:28:11 -0300

    mensagem de commit V 1.0!

---
+++ src/main/java/br/eti/agatha/airports/AirportsApplication.java
@@ -0,0 +1,12 @@
+package br.eti.agatha.airports;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirportsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirportsApplication.class, args);
+	}
+}
